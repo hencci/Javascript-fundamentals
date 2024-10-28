70e1ed55a5eb5ed7303fe668e24e960b6ad5a48f
hencci <hencci20@gmail.com>
2024-10-28 14:53:07 +0100

    Declaring variables

---
+++ Variables and operators/V-and-A.js
@@ -0,0 +1,20 @@
+// Variables are the building blocks of any program, you can think of variables as “storage containers” for data in your code.
+
+// You can declare variables using the let keyword
+let firstname = "John";
+let surname = "Doe";
+
+console.log(firstname);
+console.log(surname);
+
+// You can also re-assign variables
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+ 
+console.log(age); // what will be output now?
+
+
+// Re-assigning is cool and all, but what if we don’t want it to happen? We can accomplish this using the 'const' keyword.
+const pi = 3.14;
