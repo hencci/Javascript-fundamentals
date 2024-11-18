437b6e73c96cd44a6f71a120e4453c9539dda175
hencci <hencci20@gmail.com>
2024-11-18 15:02:14 +0100

    Create new div with h1 and p elements inside, append div to container

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -10,3 +10,17 @@ const h3 = document.createElement("h3");
 h3.textContent = "I'm a blue h3";
 h3.style.color = "blue";
 container.appendChild(h3);
+
+const newDiv = document.createElement("div");
+newDiv.style.backgroundColor = "black";
+
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+h1.style.color = "white";
+const para2 = document.createElement("p");
+para2.textContent = "ME TOO!";
+para2.style.color = "white";
+
+newDiv.appendChild(h1);
+newDiv.appendChild(para2);
+container.appendChild(newDiv);
