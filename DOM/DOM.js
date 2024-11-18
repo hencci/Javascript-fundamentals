186abacdcaa6fe3babdf2db190eda98818aaee62
hencci <hencci20@gmail.com>
2024-11-18 14:29:45 +0100

    Create and apend h3 heading

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -5,3 +5,8 @@ para.classList.add("red");//Adds class "red"
 para.textContent = "Hey I'm red!"//Updates the text
 para.style.color = "red";//Changes text color to red
 container.appendChild(para);//Adds new paragraph to container
+
+const h3 = document.createElement("h3");
+h3.textContent = "I'm a blue h3";
+h3.style.color = "blue";
+container.appendChild(h3);
